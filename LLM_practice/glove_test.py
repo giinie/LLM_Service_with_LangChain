f19455c707ff6ed4125df38b16a712ba510f734e
Gi-in Jeong <giinie72@gmail.com>
2025-07-26 08:06:39 +0900

    Convert GloVe embeddings to Word2Vec format and add sample embeddings for testing.

--- LLM_practice/glove_test.py
+++ LLM_practice/glove_test.py
@@ -1,7 +1,8 @@
+# gensim은 자연어 처리를 위한 파이썬 라이브러리로, 문서 유사성 분석을 위해 사용됩니다.
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
 
-glove_path = 'glove.6B.100d.txt'
+glove_path = '../Data/glove.6B.100d.txt'
 
 with open(glove_path, 'w') as f:
     f.write("cat 0.5 0.3 0.2\n")
@@ -12,5 +13,5 @@ word2vec_output_file = glove_path + '.word2vec'
 glove2word2vec(glove_path, word2vec_output_file)
 
 model = KeyedVectors.load_word2vec_format(word2vec_output_file, binary=False)
-print(model['cat'])
+print(model['cat']) # 'cat'에 대한 벡터
 print(model.most_similar('cat'))
